4f5da0aeb56cb1954ffc9e139cddf23ad6787a90
Ramius-arch <colinnwambugu@gmail.com>
2025-10-03 00:32:48 +0300

    Renamed files to use underscores for valid Python imports

---
+++ src/backend/controllers/flask_app_config.py
@@ -0,0 +1,10 @@
+
+from flask import Flask
+from database import db # type: ignore
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key-here'
+db.init_app(app)
+
+with app.test_request_context():
+	db.create_all()
